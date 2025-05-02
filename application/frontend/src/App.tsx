d2b86134c20ab4116d0a3b57e0904d04aaeb97c1
guxu11 <guxu19970823@126.com>
2025-05-02 03:26:43 -0700

    remove about

--- application/frontend/src/App.tsx
+++ application/frontend/src/App.tsx
@@ -1,15 +1,7 @@
-import About from "./components/About/About";
-import RuxueJ from "./components/About/RuxueJ";
-import XuG from "./components/About/XuG";
-import PasangS from "./components/About/PasangS";
-import DanteV from "./components/About/DanteV";
-import Youtheezus from "./components/About/yontheezus";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import "./App.css";
 import React from "react";
-import JunghyunS from "./components/About/JunghyunS";
-import RaymondL from "./components/About/RaymondL";
 
 import RecipeDetailMissingIngredientsPage from "./components/Recipe/RecipeDetailMissingIngredients";
 import RecipeDetailPage from "./components/Recipe/RecipeDetail";
@@ -37,14 +29,6 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/search" element={<SearchResults />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/about/RuxueJ" element={<RuxueJ />} />
-            <Route path="/about/XuG" element={<XuG />} />
-            <Route path="/about/JunghyunS" element={<JunghyunS />} />
-            <Route path="/about/PasangS" element={<PasangS />} />
-            <Route path="/about/DanteV" element={<DanteV />} />
-            <Route path="/about/RaymondL" element={<RaymondL />} />
-            <Route path="/about/YonatanL" element={<Youtheezus />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/login" element={<Login />} />
 
